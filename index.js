65f8f8be42937bc75e1e0912e783ae5f38a26056
Orander Robinson-Hartley <oranroha@gmail.com>
2021-12-09 12:16:06 -0500

    Alerts, glossary, gridpoint, stations, office, point all working. getting more stuff done as we speak.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ import https from 'https';
  * @param {number} options.limit max number of results
  * @param {string} options.cursor Pagination cursor. Don't ask me what it does b/c IDK either
  */
-export function getAlerts(options) {
+export function getAlerts(options = {}) {
     return new Promise((re, rj) => {
         let url = '/alerts?';
         if (options.start) url += `start=${options.start}&`;
@@ -45,7 +45,6 @@ export function getAlerts(options) {
         if (options.limit) url += `limit=${options.limit}&`;
         if (options.cursor) url += `cursor=${options.cursor}&`;
         url = url.slice(0, url.length - 1);
-        console.log(url);
         const o = {
             hostname: 'api.weather.gov',
             path: url,
@@ -57,55 +56,186 @@ export function getAlerts(options) {
         https.get(o, (res) => {
             let data = '';
             res.on('data', chunk => data += chunk);
-            res.on('end', () => re(data));
+            res.on('end', () => re(JSON.parse(data)));
         });
     });
 }
 /**
- * Get station or station data from NOAA
- * @param {object} options options object
- * @param {string} options.id Station ID
- * @param {boolean} observations Whether to include the /observations endpoint
- * @param {boolean} latest get latest observation
- * @param {string} time Time to get observations. Obviously requires observations.
+ * Super bloody simple. Literally no arguments; it just returns the NOAA glossary endpoint.
  */
-export function getStation(options) {
-    return new Promise((re, rj) => {
+export function getGlossary() {
+    return new Promise((rs, rj) => {
+        const o = {
+            hostname: 'api.weather.gov',
+            path: '/glossary',
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+            }
+        };
+        https.get(o, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => rs(JSON.parse(data)));
+        });
+    })
+}
+/**
+ * Get data from a grid point
+ * @param {string} wfo WFO. If using latlong, set this to null.
+ * @param {number} x Grid point x. If using latlong, this is the lattitude.
+ * @param {number} y grid point y. If using latlong, this is the longitude.
+ * @param {object} options 
+ * @param {boolean} options.forecast whether to get forecast data
+ * @param {boolean} options.forecastTempQV whether to represent forecast temp as quantitative
+ * @param {boolean} options.forecastWindQV whether to represent forecast wind as quantitative
+ * @param {string} options.units units to use (us, si);
+ * @param {boolean} options.hourly whether to make forecast data hourly
+ * @param {boolean} options.stations whether to get stations from gridpoint (incompatible with forecast)
+ * @param {boolean} options.useLatLong whether to use lat/long instead of x/y. Will need to get a gridpoint first so this call will take a while
+ */
+export function getGridPoint(wfo, x, y, options = {}) {
+    return new Promise(async (rs, rj) => {
+        let url = `/gridpoints/${wfo}/${x},${y}`;
+        if (!options.useLatLong) {
+            await new Promise(r => {
+                https.get({
+                    hostname: 'api.weather.gov',
+                    path: `/points/${x},${y}`,
+                    method: 'GET',
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+                    }
+                }, (res) => {
+                    let data = '';
+                    res.on('data', chunk => data += chunk);
+                    res.on('end', () => {
+                        const gridpoint = JSON.parse(data);
+                        x = parseInt(gridpoint.properties.gridX);
+                        y = parseInt(gridpoint.properties.gridY);
+                        wfo = gridpoint.properties.gridId;
+                        url = `/gridpoints/${wfo}/${x},${y}`;
+                        r();
+                    });
+                }).end();
+            });
+        }
+        if (options.forecast) {
+            url += '/forecast';
+            if (options.hourly) url += '/hourly';
+            if (options.forecastTempQV || options.forecastWindQV || options.units) url += '?';
+            if (options.forecastTempQV) url += 'forecast_temp_qv=true&';
+            if (options.forecastWindQV) url += 'forecast_wind_qv=true&';
+            if (options.units) url += `units=${options.units}&`;
+        } else if (options.stations) {
+            url += '/stations';
+        }
+        const o = {
+            hostname: 'api.weather.gov',
+            path: url,
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+            }
+        };
+        https.get(o, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => rs(JSON.parse(data)));
+        });
     });
 }
 /**
- * Get radar data
- * @param {object} options 
- * @param {boolean} options.servers contact the servers endpoint
- * @param {string} options.serverid server id. requires servers to be enabled.
- * @param {boolean} options.stations contact the stations endpoint
- * @param {string} options.stationid station id. requires stations or profiless to be enabled.
- * @param {boolean} options.stationalarm station alarms. requires stations and stationid.
- * @param {string} options.queueHost contact queue endpoint, and host for that.
- * @param {boolean} options.profilers contact profilers endpoint. requires stationid to function.
+ * Get station data from station id, or list of stations
+ * @param {object} options
+ * @param {boolean} options.observations whether to get observations
+ * @param {boolean} options.latest whether to get latest observations (incompatible with time)
+ * @param {string} options.time time to get observations for (incompatible with latest)
+ * @param {string} options.id station id to get data for
  */
-export function getRadar(options) {
-    return new Promise((re, rj) => {
+export function getStations(options = {}) {
+    return new Promise((rs, rj) => {
+        let url = '/stations';
+        if (options.id) {
+            url += `/${options.id}`;
+            if (options.observations) {
+                url += '/observations';
+                if (options.latest) url += '/latest';
+                else if (options.time) url += `/${options.time}`;
+            }
+        }
+        const o = {
+            hostname: 'api.weather.gov',
+            path: url,
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+            }
+        }
+        https.get(o, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => rs(data));
+        }).end();
     });
 }
 /**
- * Get zone data
- * Observations and stations are mutually exclusive.
+ * Get office data from office id
  * @param {object} options 
- * @param {string} options.type Type of zone, contacts that endpoint
- * @param {string} options.zoneid zone id
- * @param {boolean} options.forecast whether to get forecast.
- * @param {boolean} option.observations enable observations. Requires forecast.
- * @param {boolean} options.stations get stations giving forcast. requires forecast.
+ * @param {string} options.id office id to get data for
+ * @param {boolean} options.headlines whether to get headlines
+ * @param {string} options.headlineid headline id to get data for
  */
-export function getZones(options) {
-    return new Promise((re, rj) => {
+export function getOffices(options) {
+    return new Promise((rs, rj) => {
+        let url = '/offices';
+        if (options.id) url += `/${options.id}`;
+        else rj('No id provided');
+        if (options.headlines) url += '/headlines';
+        if (options.headlineid) url += `/${options.headlineid}`;
+        const o = {
+            hostname: 'api.weather.gov',
+            path: url,
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+            }
+        }
+        https.get(o, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => rs(data));
+        }).end();
     });
 }
-
+/**
+ * Get data on a point. The better version of getGridPoint.
+ * @param {object} options 
+ * @param {number} options.lat latitude of point
+ * @param {number} options.lon longitude of point
+ */
+export function getPoint(options = {}) {
+    return new Promise((rs, rj) => {
+        const o = {
+            hostname: 'api.weather.gov',
+            path: `/points/${options.lat},${options.lon}`,
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+            }
+        }
+        https.get(o, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => rs(data));
+        }).end();
+    });
+}
 export default {
     getAlerts,
-    getStation,
-    getRadar,
-    getZones
+    getGlossary,
+    getGridPoint,
+    getStations,
+    getOffices,
+    getPoint
 }
--- test.js
+++ test.js
@@ -5,10 +5,45 @@ import noaa from './index.js';
     console.log(`Check either by navigating to that endpoint in a browser, or checking at https://www.weather.gov/documentation/services-web-api`);
     console.log(`It should be noted that sometimes a 404 page can return an OK result, so make sure to double check results.`)
     try {
-        let alerts = await noaa.getAlerts({start:'2021-06-24T09:07:21-07:00', end:'2021-06-23T09:07:21-07:00'});
+        let alerts = await noaa.getAlerts({ start: '2021-06-24T09:07:21-07:00', end: '2021-06-23T09:07:21-07:00' });
         if (alerts) console.log(`ALERTS: OK`);
         else console.log(`ALERTS: FAIL - RESPONSE: ${alerts}`);
     } catch (e) {
         console.log(`ALERTS: FAIL - ERROR: ${e}`);
+    }
+    try {
+        let glossary = await noaa.getGlossary();
+        if (glossary) console.log(`GLOSSARY: OK`);
+        else console.log(`GLOSSARY: FAIL - RESPONSE: ${glossary}`);
+    } catch (e) {
+        console.log(`GLOSSARY: FAIL - ERROR: ${e}`);
+    }
+    try {
+        let gridpoint = await noaa.getGridPoint(null, 40.7, -74, { useLatLon: true, forecast: true, hourly: true });
+        if (gridpoint) console.log(`GRIDPOINT: OK`);
+        else console.log(`GRIDPOINT: FAIL - RESPONSE: ${gridpoint}`);
+    } catch (e) {
+        console.log(`GRIDPOINT: FAIL - ERROR: ${e}`);
+    }
+    try {
+        let stations = await noaa.getStations({ id: 'K12N' });
+        if (stations) console.log(`STATIONS: OK`);
+        else console.log(`STATIONS: FAIL - RESPONSE: ${stations}`);
+    } catch (e) {
+        console.log(`STATIONS: FAIL - ERROR: ${e}`);
+    }
+    try {
+        let office = await noaa.getOffices({ id: 'OKX' });
+        if (office) console.log(`OFFICE: OK`);
+        else console.log(`OFFICE: FAIL - RESPONSE: ${office}`);
+    } catch (e) {
+        console.log(`OFFICE: FAIL - ERROR: ${e}`);
+    }
+    try {
+        let point = await noaa.getPoint({ lat: 40.7, lon: -74 });
+        if (point) console.log(`POINT: OK`);
+        else console.log(`POINT: FAIL - RESPONSE: ${point}`);
+    } catch (e) {
+        console.log(`POINT: FAIL - ERROR: ${e}`);
     }
 })();
