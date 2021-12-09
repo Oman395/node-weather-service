9861ef7ed3e9c24af508b81ebf8fe851d1b2d1b9
Orander Robinson-Hartley <oranroha@gmail.com>
2021-12-09 09:43:01 -0500

    Alerts are working fully, i think at least lol

--- index.js
+++ index.js
@@ -9,95 +9,103 @@ import https from 'https';
  * Options are mutually exclusive, with the exception that zone, area, and region are all dependent on
  * active being true.
  * @param {object} options Options object
- * @param {boolean} options.active Whether or not use the /active endpoint
- * @param {boolean} options.getTypes Whether it should be getting alert types
- * @param {boolean} options.count Get count of active alerts, requires active to be true
- * @param {string} options.zone The zone to get alerts for
- * @param {string} options.area The area to get alerts for
- * @param {string} options.region The region to get alerts for
- * @param {string} options.id The ID of alerts to look for
+ * @param {boolean} options.active (dep) active only
+ * @param {string} options.start start time
+ * @param {string} options.end end time
+ * @param {array[string]} options.status status (actual, excersize, system, test, draft)
+ * @param {array[string]} options.messagetype message type (alert, update, cancel);
+ * @param {array[string]} options.event event type
+ * @param {array[string]} options.code event code
+ * @param {string} options.area State/territory area code
+ * @param {array[string]} options.region Marine region code.
+ * @param {string} options.regionType Region type (land/marine)
+ * @param {array[string]} options.zone Zone id
+ * @param {array[string]} options.urgency Urgency (immediate, expected, future, past, unknown).
+ * @param {array[string]} options.severity Severity (severe, moderate, minor, unknown).
+ * @param {array[string]} options.certainty Certainty (observed, likely, possible, unlikely, unknown).
+ * @param {number} options.limit max number of results
+ * @param {string} options.cursor Pagination cursor. Don't ask me what it does b/c IDK either
  */
 export function getAlerts(options) {
     return new Promise((re, rj) => {
-        let url = '';
-        if (options.active) {
-            if (options.zone) url = `/alerts/active/zone/${options.zone}`;
-            else if (options.area) url = `/alerts/active/area/${options.area}`;
-            else if (options.region) url = `/alerts/active/region/${options.region}`;
-            else if (options.count) url = '/alerts/active/count';
-            else url = '/alerts/active';
-        } else {
-            if (options.getTypes) url = '/alerts/types';
-            else if (options.id) url = '/alerts/id'
-            else {
-                url = '/alerts'
-            }
-        }
-        https.request({
+        let url = '/alerts?';
+        if (options.start) url += `start=${options.start}&`;
+        if (options.end) url += `end=${options.end}&`;
+        if (options.status) url += `status=${options.status.join(',')}&`;
+        if (options.messagetype) url += `message_type=${options.messagetype.join(',')}&`;
+        if (options.event) url += `event=${options.event.join(',')}&`;
+        if (options.code) url += `code=${options.code.join(',')}&`;
+        if (options.area) url += `area=${options.area}&`;
+        if (options.region) url += `region=${options.region.join(',')}&`;
+        if (options.regionType) url += `region_type=${options.regionType}&`;
+        if (options.zone) url += `zone=${options.zone.join(',')}&`;
+        if (options.urgency) url += `urgency=${options.urgency.join(',')}&`;
+        if (options.severity) url += `severity=${options.severity.join(',')}&`;
+        if (options.certainty) url += `certainty=${options.certainty.join(',')}&`;
+        if (options.limit) url += `limit=${options.limit}&`;
+        if (options.cursor) url += `cursor=${options.cursor}&`;
+        url = url.slice(0, url.length - 1);
+        console.log(url);
+        const o = {
             hostname: 'api.weather.gov',
             path: url,
             method: 'GET',
-            port: 443,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/17.3; x86_64) Gecko Firefox/43.0.4'
-            },
-        }, res => {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/18.3; x86_64) Gecko Firefox/46.0.4'  // Linux mint, x64, node lts v16
+            }
+        };
+        https.get(o, (res) => {
             let data = '';
             res.on('data', chunk => data += chunk);
-            res.once('end', () => re(data));
-        }).end();
+            res.on('end', () => re(data));
+        });
     });
 }
 /**
  * Get station or station data from NOAA
  * @param {object} options options object
  * @param {string} options.id Station ID
- * @param {boolean} headline Whether to include the /headline endpoint
- * @param {string} options.headlineId Headline ID. Dependent on id being set.
+ * @param {boolean} observations Whether to include the /observations endpoint
+ * @param {boolean} latest get latest observation
+ * @param {string} time Time to get observations. Obviously requires observations.
  */
 export function getStation(options) {
     return new Promise((re, rj) => {
-        if (options.headline) {
-            let url = '/stations/headlines'
-            if (options.headlineId) {
-                url += '/' + options.headlineId;
-            }
-            https.request({
-                hostname: 'api.weather.gov',
-                path: url,
-                method: 'GET',
-                port: 443,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/17.3; x86_64) Gecko Firefox/43.0.4'
-                },
-            }, res => {
-                let data = '';
-                res.on('data', chunk => data += chunk);
-                res.once('end', () => re(data));
-            }).end();
-        } else {
-            let url = '/stations';
-            if (options.id) {
-                url += '/' + options.id;
-            }
-            https.request({
-                hostname: 'api.weather.gov',
-                path: url,
-                method: 'GET',
-                port: 443,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (X11; Linux Mint/17.3; x86_64) Gecko Firefox/43.0.4'
-                },
-            }, res => {
-                let data = '';
-                res.on('data', chunk => data += chunk);
-                res.once('end', () => re(data));
-            });
-        }
+    });
+}
+/**
+ * Get radar data
+ * @param {object} options 
+ * @param {boolean} options.servers contact the servers endpoint
+ * @param {string} options.serverid server id. requires servers to be enabled.
+ * @param {boolean} options.stations contact the stations endpoint
+ * @param {string} options.stationid station id. requires stations or profiless to be enabled.
+ * @param {boolean} options.stationalarm station alarms. requires stations and stationid.
+ * @param {string} options.queueHost contact queue endpoint, and host for that.
+ * @param {boolean} options.profilers contact profilers endpoint. requires stationid to function.
+ */
+export function getRadar(options) {
+    return new Promise((re, rj) => {
+    });
+}
+/**
+ * Get zone data
+ * Observations and stations are mutually exclusive.
+ * @param {object} options 
+ * @param {string} options.type Type of zone, contacts that endpoint
+ * @param {string} options.zoneid zone id
+ * @param {boolean} options.forecast whether to get forecast.
+ * @param {boolean} option.observations enable observations. Requires forecast.
+ * @param {boolean} options.stations get stations giving forcast. requires forecast.
+ */
+export function getZones(options) {
+    return new Promise((re, rj) => {
     });
 }
 
 export default {
     getAlerts,
     getStation,
+    getRadar,
+    getZones
 }
--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 import noaa from './index.js';
 (async () => {
-    let result = await noaa.getAlerts({});
-    console.log(result);
+    console.log(`Testing endpoints...`);
+    console.log(`If anything fails, it's around a 50/50 that the endpoint is down.`);
+    console.log(`Check either by navigating to that endpoint in a browser, or checking at https://www.weather.gov/documentation/services-web-api`);
+    console.log(`It should be noted that sometimes a 404 page can return an OK result, so make sure to double check results.`)
+    try {
+        let alerts = await noaa.getAlerts({start:'2021-06-24T09:07:21-07:00', end:'2021-06-23T09:07:21-07:00'});
+        if (alerts) console.log(`ALERTS: OK`);
+        else console.log(`ALERTS: FAIL - RESPONSE: ${alerts}`);
+    } catch (e) {
+        console.log(`ALERTS: FAIL - ERROR: ${e}`);
+    }
 })();
